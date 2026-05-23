ec66fcd93f5418d6995baa9c8fdd3365bea7d064
pirosiki197 <lingmukangtai825@gmail.com>
2026-05-23 22:11:15 +0900

    fix: scroll to top of the page when navigating

--- apps/web/src/layouts/RootLayout.tsx
+++ apps/web/src/layouts/RootLayout.tsx
@@ -1,10 +1,13 @@
 import Header from '@/components/Haeder'
 import MobileNav from '@/components/MobileNav'
+import ScrollToTop from '@/components/ScrollToTop'
 import { Outlet } from 'react-router-dom'
 
 function RootLayout() {
   return (
     <div className="bg-background pb-15">
+      <ScrollToTop />
+
       <Header />
 
       <div className="mx-auto min-h-screen max-w-2xl">
